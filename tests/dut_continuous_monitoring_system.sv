1aedafedb0940030f64699e4e8f53ca86adea6b1
michalmonday <michalmonday17@gmail.com>
2022-12-16 17:06:27 -0800

    Updated simulation (to view pc and instr values in AXI tdata)

--- tests/dut_continuous_monitoring_system.sv
+++ tests/dut_continuous_monitoring_system.sv
@@ -2,10 +2,11 @@
 
 `define ADDR_WIDTH 8 // internal addressing (each of 256 addresses can result in a different action upon writing/reading)
 `define DATA_WIDTH 64 // control data width, the functionality of the module is controlled by writing to address+data ports
+import continuous_monitoring_system_pkg::*;
 
 module dut_continuous_monitoring_system;
-    localparam XLEN = 64;
-    localparam AXI_DATA_WIDTH = 1024;
+    // localparam XLEN = 64;
+    // localparam AXI_DATA_WIDTH = 1024;
     localparam period = 10;
     localparam clk_period = 5;
 
@@ -18,7 +19,7 @@ module dut_continuous_monitoring_system;
     reg [31:0] instr;
     reg pc_valid=1;
 
-    reg [`ADDR_WIDTH-1:0]ctrl_addr = 0;
+    ctrl_addr_t ctrl_addr = TRIGGER_TRACE_START_ADDRESS_ENABLED;
     reg [`DATA_WIDTH-1:0]ctrl_wdata = 0;
     reg ctrl_write_enable = 0;
 
@@ -26,6 +27,11 @@ module dut_continuous_monitoring_system;
 
     localparam INPUT_EVENT_BITMAP_WIDTH = 115;
     reg [INPUT_EVENT_BITMAP_WIDTH-1:0] performance_events = 0; // bitmap
+
+    localparam PC_LOCATION = 115*7;
+    localparam INSTR_LOCATION = PC_LOCATION + 64 + 64;
+    wire [63:0]tdata_pc = M_AXIS_tdata[PC_LOCATION+63:PC_LOCATION];
+    wire [31:0]tdata_instr = M_AXIS_tdata[INSTR_LOCATION+31:INSTR_LOCATION];
 
 
     continuous_monitoring_system #(
@@ -68,7 +74,7 @@ module dut_continuous_monitoring_system;
         i = i + 1;
         case (i)
             0:  begin 
-                instr = 32'h00000000; // nop
+                instr = 32'h00000001; // nop
                 performance_events = 8'b10101010;
             end
             1:  begin 
@@ -76,69 +82,77 @@ module dut_continuous_monitoring_system;
                 performance_events = 8'b10101010;
             end
             2:  begin 
-                instr = 32'h0C601063; // blt     a0, a1, .LBB0_2
+                instr = 32'h00000001; // nop
                 performance_events = 8'b10101010;
             end
             3:  begin 
+                instr = 32'h00000001; // nop
+                performance_events = 8'b10101010;
+            end
+            4:  begin 
+                instr = 32'h0C601063; // blt     a0, a1, .LBB0_2
+                performance_events = 8'b10101010;
+            end
+            5:  begin 
                 instr = 32'h00000067; // riscv jalr instruction
                 performance_events = 8'b01010101;
             end
-            4:  begin 
-                instr = 32'h00000000; // nop
-                performance_events = 8'b00000001;
-            end
-            5:  begin 
-                instr = 32'h000000ef; // riscv jal instruction
-                performance_events = 8'b00000001;
-            end
             6:  begin 
-                instr = 32'h00130013; // addi t0, t1, 10
+                instr = 32'h00000000; // nop
                 performance_events = 8'b00000001;
             end
             7:  begin 
-                instr = 32'h00000000; // nop
+                instr = 32'h000000ef; // riscv jal instruction
                 performance_events = 8'b00000001;
             end
             8:  begin 
-                instr = 32'h0000006f; // riscv branch instruction
+                instr = 32'h00130013; // addi t0, t1, 10
                 performance_events = 8'b00000001;
             end
             9:  begin 
+                instr = 32'h00000000; // nop
+                performance_events = 8'b00000001;
+            end
+            10:  begin 
+                instr = 32'h0000006f; // riscv branch instruction
+                performance_events = 8'b00000001;
+            end
+            11:  begin 
                 instr = 32'h0C601063; // blt     a0, a1, .LBB0_2
             end
 
-            10: begin 
+            12: begin 
                  instr = 32'h10500073; // WFI (wait for interrupt)
             end
-            11: begin 
+            13: begin 
                  instr = 32'h10500073; // WFI (wait for interrupt)
             end
 
-            12: begin 
-                 instr = 32'h0000006f; // riscv branch instruction
-            end
-            13: begin 
-                 instr = 32'h0C601063; // blt     a0, a1, .LBB0_2
-            end
             14: begin 
-                 instr = 32'h00000067; // riscv jalr instruction
+                 instr = 32'h0000006f; // riscv branch instruction
             end
             15: begin 
-                 instr = 32'h00000000; // nop
+                 instr = 32'h0C601063; // blt     a0, a1, .LBB0_2
             end
             16: begin 
-                 instr = 32'h000000ef; // riscv jal instruction
+                 instr = 32'h00000067; // riscv jalr instruction
             end
             17: begin 
-                 instr = 32'h00130013; // addi t0, t1, 10
-            end
-            18: begin 
                  instr = 32'h00000000; // nop
             end
+            18: begin 
+                 instr = 32'h000000ef; // riscv jal instruction
+            end
             19: begin 
-                 instr = 32'h0000006f; // riscv branch instruction
+                 instr = 32'h00130013; // addi t0, t1, 10
             end
             20: begin 
+                 instr = 32'h00000000; // nop
+            end
+            21: begin 
+                 instr = 32'h0000006f; // riscv branch instruction
+            end
+            22: begin 
                  instr = 32'h0C601063; // blt     a0, a1, .LBB0_2
             end
             default: begin
